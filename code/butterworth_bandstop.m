cc1099a5481394839abf8ece432d7d4519a254de
Sankul2699 <63014688+Sankul2699@users.noreply.github.com>
2020-04-14 00:46:11 +0530

    Create butterworth_bandstop.m

---
+++ code/butterworth_bandstop.m
@@ -0,0 +1,19 @@
+fs=8000;
+[y ,fs]= audioread('voice2.wav');
+subplot(2,1,1);
+c=fft(y);
+plot(abs(c))
+
+xlabel('Frequency');
+ylabel('amplitude');
+title('MY VOICE');
+
+[b,a]=butter(10,[0.3,0.6],'stop');
+x=filter(b,a,y);
+subplot(2,1,2);
+d=fft(x);
+plot(abs(d))
+title('filtered signal');
+xlabel('Frequency');
+ylabel('amplitude');
+audiowrite('bstop.wav',x,fs);
